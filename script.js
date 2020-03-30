5bf480a01b441979c6857529a25c2f8a51cb5e89
mrvnre <mrvnre24@gmail.com>
2020-03-30 16:21:53 -0500

    Added alerts and updated shuttle requirements.

--- script.js
+++ script.js
@@ -11,3 +11,47 @@
 </ol>
 <img src="${}">
 */
+window.addEventListener("load", function() {
+   let launchForm = document.getElementById("launchForm");
+   launchForm.addEventListener("submit", function(event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName =  document.querySelector("input[name=copilotName]");
+      let fuelLevel =  document.querySelector("input[name=fuelLevel]");
+      let cargoMass =  document.querySelector("input[name=cargoMass]");
+         if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "")  {
+            alert("All fields are required!");
+            event.preventDefault();
+         }
+         if (isNaN(pilotName.value) === false || isNaN(copilotName.value) === false || isNaN(fuelLevel.value) === true || isNaN(cargoMass.value) === true) {
+            alert("Make sure to enter valid information for each field!");
+            event.preventDefault();
+         }
+         
+      let faultyId = document.getElementById("faultyItems");
+     
+   let fuelId = document.getElementById("fuelStatus");   
+   let launchStatusId = document.getElementById("launchStatus");
+   faultyId.style.visibility = "visible";
+   faultyId.innerHTML = `<ol>
+         <li>Pilot ${pilotName.value} Ready</li>
+         <li>Co-pilot ${copilotName.value} Ready</li>
+         <li>${fuelId.innerHTML}</li>
+         <li>Cargo mass low enough for launch</li>
+     </ol>`;
+
+         if (fuelLevel.value < 10000) {
+              fuelId.style.visibility = "visible";
+            launchStatusId.innerHTML = "Shuttle not ready for launch";
+            launchStatusId.style.color = "red";
+            fuelId.innerHTML = "Fuel level too low for launch."; 
+           } else {
+              fuelId.style.visibility = "visible";
+              fuelId.innerHTML = "Fuel level high enough for launch";
+           }
+     
+   });
+   
+   
+                    
+});   
+
